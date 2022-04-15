b8b39ebd23a8013723a898b4a715dfbc68d092cc
VladimirZvezdkin <vladimir-zvezdkin@yandex.ru>
2022-04-15 18:10:32 +0300

    rename .ini file add By in test_fixture8

--- test_fixture8.py
+++ test_fixture8.py
@@ -1,4 +1,5 @@
 import pytest
+from selenium.webdriver.common.by import By
 from selenium import webdriver
 
 link = "http://selenium1py.pythonanywhere.com/"
@@ -18,9 +19,9 @@ class TestMainPage1():
     @pytest.mark.smoke
     def test_guest_should_see_login_link(self, browser):
         browser.get(link)
-        browser.find_element_by_css_selector("#login_link")
+        browser.find_element(by=By.CSS_SELECTOR,value="#login_link")
 
     @pytest.mark.regression
     def test_guest_should_see_basket_link_on_the_main_page(self, browser):
         browser.get(link)
-        browser.find_element_by_css_selector(".basket-mini .btn-group > a")
+        browser.find_element(by=By.CSS_SELECTOR, value=".basket-mini .btn-group > a")
